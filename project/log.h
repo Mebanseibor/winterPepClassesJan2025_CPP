ae466d78760c3a780e745413071679bf46c1910c
Mebanseibor Lyngdoh <mebanseiborlm@gmail.com>
2025-02-12 23:45:25 +0530

    Added logging functionality

---
+++ project/log.h
@@ -0,0 +1,38 @@
+#ifndef logLib
+    #define logLib
+
+    #include <string>
+    #include <ctime>
+    #include <chrono>
+    #include <fstream>
+
+    namespace loglib{
+        class Log{
+            std::chrono::time_point<std::chrono::system_clock> time;
+            std::string message;
+            std::fstream logFileWriter;
+        
+        public:
+            Log(){
+                this->logFileWriter.open("log file.txt", std::ios::app);
+            }
+
+            bool isLogFileOpen(){
+                if(logFileWriter.is_open()) return true;
+                return false;
+            };
+            
+            void writeLog(std::string message){
+                if(!isLogFileOpen())return;
+                
+                logFileWriter  << "[" << "time" << "]\t" << message << "\n";  // outputs the content to the linked file
+            }
+
+            ~Log(){
+                writeLog("Destroying log object");
+                logFileWriter.close();
+            }
+        };
+    }
+
+#endif
--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "log.h"
 
 int main(){
-    
+    loglib::Log log;
+    log.writeLog("Created a log object");
 };
